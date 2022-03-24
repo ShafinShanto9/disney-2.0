42ac5ee1022ffdef4ee3b86729ec653f31442593
shafin_shant0 <shafinshanto9@gmail.com>
2022-03-24 16:49:28 +0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const Home  = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Disney 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>
+          Disney Clone 
+        </h1>
+      </main>
+      
+    </div>
+  )
+}
+
+export default Home
